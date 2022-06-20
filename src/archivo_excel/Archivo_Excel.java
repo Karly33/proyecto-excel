1dc0db24917da82c60271fda428a54d2009ac5d7
JM <JM@KARLY->
2022-06-20 03:12:19 -0700

    PROYECTO 3.1

---
+++ src/archivo_excel/Archivo_Excel.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package archivo_excel;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+
+
+/**
+ *
+ * @author JM
+ */
+public class Archivo_Excel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     // TODO code application logic hereWorkbook workbook = new HSSFWorkbook();
+        //Crea hoja nueva
+        Sheet sheet= workbook.createSheet("Hoja de datos");
+        //Por cada línea se crea un arreglo de objetos (Object[])
+        Map<String, Object[]> datos = new TreeMap<String, Object[]>();
+        datos.put("1", new Object[]{"Identificador", "Nombre", "Apellidos"});
+        datos.put("2", new Object[]{1, "María", "Remen"});
+        datos.put("3", new Object[]{2, "David", "Allos"});
+        datos.put("4", new Object[]{3, "Carlos", "Caritas"});
+        datos.put("5", new Object[]{4, "Luisa", "Vitz"});
+        //Iterar sobre datos para escribir en la hoja
+        Set keyset = datos.keySet();
+        int numeroRenglon = 0;
+        for (Object key : keyset) {
+            Row row = sheet.createRow(numeroRenglon++);
+            Object[] arregloObjetos = datos.get(key);
+            int numeroCelda = 0;
+            for (Object obj : arregloObjetos) {
+            Cell cell = row.createCell(numeroCelda++);
+            if (obj instanceof String) {
+            cell.setCellValue((String) obj);
+            } else if (obj instanceof Integer) {
+            cell.setCellValue((Integer) obj);
+            }
+            }
+        }
+        try {
+            //Se genera el documento
+            FileOutputStream out = new FileOutputStream(new File("/ruta/ejemplo.xlsx"));
+            workbook.write(out);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
+    
+    
+
